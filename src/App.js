560e4f8b38c5442486eda56613597943c7c5073a
Stevie IsMagic <stevieismagic@gmail.com>
2021-02-21 18:25:57 -0800

    load login state from session

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Auth } from "aws-amplify";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Router } from "react-router-dom";
@@ -8,43 +9,64 @@ import Routes from "./Routes";
 import { AppContext } from "./libs/contextLib";
 
 function App() {
+  // we start with value true b/c as we first load, app will start
+  // by checking the current authenitication state
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
   const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    } catch (e) {
+      if (e !== "No current user") {
+        alert(e);
+      }
+    }
+    setIsAuthenticating(false);
+  }
 
   function handleLogout() {
     userHasAuthenticated(false);
   }
   return (
-    <div className="App container py-3">
-      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <LinkContainer to="/">
-          <Navbar.Brand className="font-weight-bold text-muted">
-            Stevie Notes
-          </Navbar.Brand>
-        </LinkContainer>
-        <Navbar.Toggle />
-        {/* Navbar.Collapse component ensures that on mobile devices, the two links will be collapsed */}
-        <Navbar.Collapse className="justify-content-end">
-          {/* activekey is set to the current path. this will highlight the link when we are on that page */}
-          <Nav activeKey={window.location.pathname}>
-            {isAuthenticated ? (
-              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-            ) : (
-              <>
-                <LinkContainer to="/signup">
-                  <Nav.Link>SignUp</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to="/login">
-                  <Nav.Link>Login</Nav.Link>
-                </LinkContainer>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
-        <Routes />
-      </AppContext.Provider>
-    </div>
+    !isAuthenticating && (
+      <div className="App container py-3">
+        <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+          <LinkContainer to="/">
+            <Navbar.Brand className="font-weight-bold text-muted">
+              Stevie Notes
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle />
+          {/* Navbar.Collapse component ensures that on mobile devices, the two links will be collapsed */}
+          <Navbar.Collapse className="justify-content-end">
+            {/* activekey is set to the current path. this will highlight the link when we are on that page */}
+            <Nav activeKey={window.location.pathname}>
+              {isAuthenticated ? (
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              ) : (
+                <>
+                  <LinkContainer to="/signup">
+                    <Nav.Link>SignUp</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <Nav.Link>Login</Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+          <Routes />
+        </AppContext.Provider>
+      </div>
+    )
   );
 }
 
